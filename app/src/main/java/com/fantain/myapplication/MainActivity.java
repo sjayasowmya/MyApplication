a3d26f630a485ccefab456f69860238869ff32a7
Jayasowmya <jayasowmya@fantain.com>
2016-08-24 11:05:16 +0530

    Another test

--- app/src/main/java/com/fantain/myapplication/MainActivity.java
+++ app/src/main/java/com/fantain/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("TAG","Hello, World");
+        Log.i("TAG","Another Hello, World");
     }
 }
